a103b25062c3a91db42b076fb0f8db0a052e27b1
a3qz <rmichalec14@gmail.com>
2017-05-07 14:19:38 -0400

    can now only collect the right card

--- collectable.py
+++ collectable.py
@@ -27,6 +27,37 @@ class Collectable(sprite.Sprite):
         self.firing = False
         self.dead_ticks = 0
 
+    def collectable_check(self, new_desc):
+        #ok, so, we delimit suit and rank by... a 9...
+        ugh = str(self.descriptor)
+        #but it's always of the form 9S9R or 9S9RR
+        #where S is suit [1-4] and R/RR is rank [1-13]
+        # in this one we also check if opposite suit
+        #so, calculate our new stuff
+        newsuit = ugh[1]
+        if ugh[-2] == '9':
+            newrank = ugh[-1]
+        else:
+            newrank = ugh[-2:]
+        #then do the thing
+        newrank = str(int(newrank)+1) #why ryan why
+        #then do the thing
+        newcollect = '9' + newsuit + '9' + newrank
+        if (new_desc[1] == '1' or new_desc[1] == '4'):
+            suitlooking = 'r'
+        else:
+            suitlooking = 'b'
+
+        if (suitlooking == 'b' and (newcollect[1] == '1' or newcollect[1] == '4')) or (suitlooking == 'r' and (newcollect[1] == '2' or newcollect[1] == '3')):
+            print 'ere'
+            return new_desc[2:] == newcollect[2:]
+        return False
+            
+
+
+
+
+
     def gotoDead(self):
         #ok, so, we delimit suit and rank by... a 9...
         ugh = str(self.descriptor)
@@ -61,7 +92,12 @@ class Collectable(sprite.Sprite):
             self.thing_at(player.Ship, 0, 0))
 
         if w:
-            self.gotoDead()
+            if self.collectable_check(self.game.player.descriptor):
+                self.gotoDead()
+            else:
+                print '\a'
+                print 'cant collect'
+                #eventually put bad animation here
 
     def draw(self):
         self.game.screen.blit(self.img, self.rect.move(-self.game.player.viewx1, 0))
