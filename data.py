fdd9ef96c2d942e816e22d318e3c75b18574a6b3
a3qz <rmichalec14@gmail.com>
2017-05-08 22:36:01 -0400

    commented data

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import json
 
+# loads in the two json libraries that we use as reference: {card_descriptor: card_imagename} and the other way around for use in loading images
 fp1 = open("num_as_key.json")
 fp2 = open("str_as_key.json")
 num_as_key = json.load(fp1)
