cdce5adfa0770804aacfade35119b21a5759a30b
a3qz <rmichalec14@gmail.com>
2017-05-08 21:40:38 -0400

    commended constants

--- constants.py
+++ constants.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3 -B
+# size of board
 SIZE = WIDTH, HEIGHT = 656, 656
+# frames per second
 FPS = 60
+# colors we need 
 BLACK = (0, 0, 0)
 GREEN = (0, 128, 0)
 RED = (255, 0, 0)
 GRAY = (177, 177, 177)
-
+# time to spend dead
 DEAD_TIME = 60*5
+# time to stay invalid 
 INVALID_COLLECT_TIMER = 60*.5
+# macros for modes
 VERSUS = 0
 COOPERATIVE = 1
+# number of levels
 NUM_LEVELS = 5
