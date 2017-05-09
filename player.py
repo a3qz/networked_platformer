ed7739d26636b276fd4697f1b097721a5da8d7f9
a3qz <rmichalec14@gmail.com>
2017-05-08 22:18:48 -0400

    commentd player

--- player.py
+++ player.py
@@ -9,11 +9,14 @@ import constants
 import wall
 import spikes
 
+# this contains the classes for our players and our "other player" - shadow is the network player, ship is the one the player controls
 
+# network player class
 class Shadow(sprite.Sprite):
     def __init__(self, game, uid):
         super(Shadow, self).__init__(game)
         self.num = random.randint(2,4)
+        # all computer players appear as jokers
         self.img = pygame.image.load('imgs/cards/smaller_pngs/black_joker.png').convert_alpha()
         self.normal = self.img
         self.jumping = pygame.image.load('imgs/cards/final_jump/black_joker.png').convert_alpha()
@@ -21,13 +24,17 @@ class Shadow(sprite.Sprite):
         self.uid = uid
         self.rect.inflate_ip(100, 145)
 
+    # blit your image to the screen offset by the player's view window
     def draw(self):
         self.game.screen.blit(self.img,
                               self.rect.move(*self.game.player.view))
 
+    # do the movement, mostly checking for collisions
     def tick(self):
+        # make sure that the shadow does not run through a wall
         w = self.thing_at(wall.Wall, self.vx, 0) or (
             self.thing_at(Shadow, self.vx, 0))
+        # if colliding with wall stop it
         if w:
             if w.rect.x > self.rect.x:
                 self.rect.right = w.rect.left
@@ -37,9 +44,11 @@ class Shadow(sprite.Sprite):
         else:
             self.rect.x += self.vx
 
+        # make sure it does not fall through a wall
         w = self.thing_at(wall.Wall, 0, self.vy) or (
             self.thing_at(Shadow, 0, self.vy)) or (
             self.thing_at(Ship, 0, self.vy))
+        # if on a wall stop it
         if w:
             if self.vy > 0:
                 self.rect.bottom = w.rect.top
@@ -58,12 +67,15 @@ class Shadow(sprite.Sprite):
         else:
             self.img = self.normal
 
+
+# this is the class for the player
 class Ship(sprite.Sprite):
     def __init__(self, game, x, y, descriptor):
         super(Ship, self).__init__(game)
         self.card_count = 0
         self.rect.move_ip(x, y)
         self.descriptor = descriptor
+        # load in the image of the descriptor
         self.img = pygame.image.load('imgs/cards/smaller_pngs/{}'.format(data.num_as_key[descriptor])).convert_alpha()
         self.normal = self.img
         self.jumping = pygame.image.load('imgs/cards/final_jump/{}'.format(data.num_as_key[descriptor])).convert_alpha()
@@ -72,21 +84,22 @@ class Ship(sprite.Sprite):
         self.view = (0, 0)
         self.rect.inflate_ip(100, 145)
         self.keys = 0
-        #self.img = pygame.image.load('imgs/cards/players/player.png').convert_alpha()
-        #self.normal = self.img
-        #self.jumping = pygame.image.load('imgs/cards/final_jump/jack_of_hearts2.png').convert_alpha()
         self.firing = False
         self.dead_ticks = 0
 
+    #kill self for a small amount of time
     def gotoDead(self):
         Death(self.game, self.img, self.rect.centerx, self.rect.centery)
         self.dead_ticks = constants.DEAD_TIME
 
+    # increment self
     def tick(self):
+        # if dead, increment 
         if self.dead_ticks > 0:
             self.dead_ticks -= 1
             self.rect.x = 300
             self.rect.y = 4444
+            # if should not be dead, bring back to life
             if self.dead_ticks == 0:
                 self.rect.x = self.xstart
                 self.rect.y = self.ystart
@@ -165,6 +178,7 @@ class Ship(sprite.Sprite):
         elif k == 's':
             self.keys &= ~8
 
+    # handle jumping
     def fly(self):
         self.vx = (((self.keys & 2)>>1) - ((self.keys & 1)>>0)) * 7
 
@@ -174,6 +188,7 @@ class Ship(sprite.Sprite):
     def handleMUP(self, xxt, yyt):
         self.firing = False
     
+    # force it to change to whatever we want (used for reset) 
     def force_collect(self, descriptor):
         self.descriptor = descriptor
         self.img = pygame.image.load('imgs/cards/smaller_pngs/{}'.format(data.num_as_key[descriptor])).convert_alpha()
@@ -181,12 +196,14 @@ class Ship(sprite.Sprite):
         self.jumping = pygame.image.load('imgs/cards/final_jump/{}'.format(data.num_as_key[descriptor])).convert_alpha()
 
 
+    # check if a card is collectable and load it
     def collect(self, descriptor):
         if int((str(descriptor)[3:])) > int((str(self.descriptor)[3:])):
             return
         else:
             self.force_collect(descriptor)
 
+# sprite for death animation
 class Death(sprite.Sprite):
     def __init__(self, game, img, x, y):
         super(Death, self).__init__(game)
@@ -200,8 +217,10 @@ class Death(sprite.Sprite):
         self.width = 100
         self.timer = 10
 
+    # amimate ourself for the flip
     def tick(self):
         self.img = pygame.transform.scale(self.imgs, (self.width, 145))
+        # check state and adjust timer
         if self.state == 0: #frozen
             self.timer -= 1
             if self.timer < 0:
