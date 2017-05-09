ae829186a238d9ba8b5474ce9842581fb36f4d6a
a3qz <rmichalec14@gmail.com>
2017-05-08 22:32:58 -0400

    commented spikes

--- spikes.py
+++ spikes.py
@@ -6,11 +6,13 @@ import math
 import sprite
 import wall
 
+# class for the spikes that hurt
 class Spike(wall.Terrain):
     def __init__(self, game, x, y, descriptor):
         super(Spike, self).__init__(game, x, y, descriptor)
         self.descriptor = "1"
 
+    # render the lines in a triangle 
     def draw(self):
         xx = self.game.player.view[0]
         yy = self.game.player.view[1]
@@ -19,6 +21,7 @@ class Spike(wall.Terrain):
                   (16, 32),
                   (24, 0),
                   (32, 32)]
+        # draw the lines to form the points
         pygame.draw.lines(self.game.screen, (255, 255, 255), True,
                          [(self.rect.x+x[0]+xx, self.rect.y+x[1]+yy)
                           for x in points], 1)
